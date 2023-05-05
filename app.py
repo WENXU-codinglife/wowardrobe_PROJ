dd394a6233c78c7c54f7f6bd220697babf533d66
wenxuw <xu_wen@sfu.ca>
2023-05-04 20:21:19 -0700

    user register/login/access token

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
-import models
 
 from resources.items import blp as ItemsBlueprint
 from resources.wardrobes import blp as WardrobesBlueprint
@@ -23,6 +23,13 @@ def create_app(db_url=None):
     db.init_app(app)
 
     api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "sean"
+    jwt = JWTManager(app)
+    '''
+    Should generate a long and random secret key using something 
+    like str(secrets.SystemRandom().getrandbits(128)).
+    '''
 
     with app.app_context():
         db.create_all()
--- resources/users.py
+++ resources/users.py
@@ -2,44 +2,64 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import create_access_token
+from passlib.hash import pbkdf2_sha256
 from schemas import UserSchema
 from models import UsersModel
 from db import db
 
 blp = Blueprint("Users", "users", description = "Operations on users")
 
-
-@blp.route("/user")
-class Users(MethodView):
-    # get all users
-    @blp.response(200, UserSchema(many=True))
-    def get(self):
-        return UsersModel.query.all()
-
-    # create a new user
+@blp.route("/register")
+class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     @blp.response(201, UserSchema)
     def post(self, request):
+        user_email = request["user_email"]
+        user_name = request["user_name"]
+        user_password = request["user_password"]
+        if UsersModel.query.filter_by(user_email = user_email).first():
+            abort(409, message="A user with that username already exists.")
+
         user_id = uuid.uuid4().hex
         while UsersModel.query.get(user_id):
             user_id = uuid.uuid4().hex
 
-        user_name = request["user_name"]
-        user_email = request["user_email"]
-        user_password = request["user_password"]
         new_user = UsersModel(**{
             "user_id": user_id, 
             "user_name": user_name,
             "user_email": user_email,
-            "user_password": user_password
+            "user_password": pbkdf2_sha256.hash(user_password)
         })
         db.session.add(new_user)
         db.session.commit()
         return new_user
 
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, request):
+        user = UsersModel.query.filter(
+            UsersModel.user_email == request["user_email"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(request["user_password"], user.user_password):
+            access_token = create_access_token(identity=user.user_id)
+            return {"access_token": access_token}, 200
+
+        abort(401, message="Invalid credentials.")        
+
+@blp.route("/user")
+class Users(MethodView):
+    # get all users - ONLY FOR TESTING
+    @blp.response(200, UserSchema(many=True))
+    def get(self):
+        return UsersModel.query.all()
+
+
 @blp.route("/user/<string:user_id>")
 class User(MethodView):
-    # get a user
+    # get a user - ONLY FOR TESTING
     @blp.response(200, UserSchema)
     def get(self, user_id):
         user = UsersModel.query.get_or_404(user_id) # get_or_404 takes primary key as argument
